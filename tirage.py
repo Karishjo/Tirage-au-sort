bf35d1da4371d22c293a183921776e863eaae20a
KarishJo <karishma_jogoo@hotmail.com>
2025-12-17 22:00:26 +0400

    Update index.html

--- tirage.py
+++ tirage.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/") #default route
 def home():
-    return "Tirage <h1>Hello<h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
